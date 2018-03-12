e72c0809d07d2920d46359d53282b662309017d3
Saugat Dhungel <saugat.dhungel@gmail.com>
2018-03-12 09:44:37 -0400

    Added routes

--- src/components/App.js
+++ src/components/App.js
@@ -6,10 +6,15 @@ const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
 const Landing = () => <h2>Landing</h2>
 
-const App = () => {
+function App() {
   return(
     <div>
       <BrowserRouter>
+        <div>
+          <Route exact path="/" component={Landing}/>
+          <Route exact path="/surveys" component={Dashboard}/>
+          <Route path="/surveys/new" component={SurveyNew}/>
+        </div>
       </BrowserRouter>
     </div>
   )

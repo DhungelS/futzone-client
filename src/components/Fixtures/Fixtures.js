c56b1ac8352341ca21f718dcecd590e2add7c63f
Emanuel Quimper <quimperemanuel@gmail.com>
2018-03-14 14:44:18 -0400

    Change ui + fix reducer

--- src/components/Fixtures/Fixtures.js
+++ src/components/Fixtures/Fixtures.js
@@ -10,66 +10,54 @@ export class Fixtures extends Component {
     this.state = {
       showLeagues: true,
       showTeams: false,
-      showMatches: false
+      showMatches: false,
+      selectedLeague: ''
     };
   }
 
   componentDidMount() {
-    this.props.getSoccerData('https://api.football-data.org/v1/competitions');
+    this.props.getLeagues();
   }
 
-  handleLeagueSelect(link) {
-    this.props.getSoccerData(link);
+  handleLeagueSelect(link, id) {
     this.setState({
-      showTeams: true
+      showTeams: true,
+      selectedLeague: id
+    }, () => {
+
+      this.props.getTeams(link, id);
     });
   }
 
   render() {
-    const leagues = this.props.leagues.map((league, index) => {
-      return (
-        <li
-          key={index}
-          onClick={() => this.handleLeagueSelect(league._links.teams.href)}
-          className="leagues"
-        >
-          <a>{league.caption}</a>
-        </li>
-      );
-    });
-
-    const teams = this.props.teams.map((team, index) => {
-      return (
-        <li key={index} className="teams">
-          {team.name}
-        </li>
-      );
-    });
-
     return (
-      console.log(this.props.teams),
       (
         <div className="fixtures">
           <h1>Leagues</h1>
-          <table>
-            {/* {leagues}
-            {this.state.showTeams ? teams : null} */}
-            <tr>
-              <th>League Name</th>
-              <th>Team Names</th>
-            </tr>
-            <tr>
+
+          <div className="list">
+            <div className="leagues-list">
               {this.props.leagues.map((league, index) => (
-                <tr key={index}>
-                <td onClick={() => this.handleLeagueSelect(league._links.teams.href)}>{league.caption}</td>
-                {this.state.showTeams && (
-                <td>{this.props.teams[index].name}</td>
-                )}
-          
-                </tr>
+                <li
+                  key={league.id}
+                  onClick={() =>
+                    this.handleLeagueSelect(league._links.teams.href, league.id)
+                  }
+                  className="league"
+                >
+                  <a>{league.caption}</a>
+                </li>
               ))}
-            </tr>
-          </table>
+            </div>
+            <div className="teams-list">
+              {this.state.showTeams && this.props.teams[this.state.selectedLeague] &&
+                this.props.teams[this.state.selectedLeague].map((team, index) => (
+                  <li key={index} className="team">
+                    {team.name}
+                  </li>
+                ))}
+            </div>
+          </div>
         </div>
       )
     );
@@ -78,7 +66,7 @@ export class Fixtures extends Component {
 
 const mapStateToProps = state => ({
   leagues: state.soccerData.leagueData,
-  teams: state.soccerData.teamData
+  teams: state.soccerData.teamData,
 });
 
 export default connect(mapStateToProps, actions)(Fixtures);
--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
-import {createStore, applyMiddleware} from 'redux';
-import reduxThunk from 'redux-thunk';
-import './index.css'
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { createLogger } from 'redux-logger';
 
-import App from './components/App'
+import './index.css';
+import App from './components/App';
 import reducers from './reducers';
 
-const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+const middlwares = [createLogger(), thunk];
+
+const store = createStore(
+  reducers,
+  {},
+  compose(
+    applyMiddleware(...middlwares),
+    window.devToolsExtension ? window.devToolsExtension() : f => f,
+  ),
+);
 
 ReactDOM.render(
-<Provider store={store}>
-<App/>
-</Provider>, 
-document.getElementById('root')
-)
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
